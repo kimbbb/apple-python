06515358d0a685da235bbbe013054698a6db958e
김민지 <wehouse201@naver.com>
2023-03-06 16:34:37 +0900

    list

--- apple.py
+++ apple.py
@@ -6,3 +6,14 @@ print(123+456)
 fullname='슈퍼 에이전트 하이퍼 초필살 드래곤'
 print(fullname)
 print('차은우'[0])
+
+중고차=['k5', 'white', 5000]
+중고차[1]='black'
+print(중고차)
+
+중고차2={
+    'brand':'bmw',
+    'model':'520d'
+}
+중고차2['brand']=123
+print(중고차2['brand'])
