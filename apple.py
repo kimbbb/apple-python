486114fe385974abb58b143de1b828914a6dfe1d
김민지 <wehouse201@naver.com>
2023-03-07 11:30:28 +0900

    if

--- apple.py
+++ apple.py
@@ -1,19 +1,31 @@
-print(123)
-print('차은우')
-print("차은우")
-print(123+456)
+# print(123)
+# print('차은우')
+# print("차은우")
+# print(123+456)
+#
+# fullname='슈퍼 에이전트 하이퍼 초필살 드래곤'
+# print(fullname)
+# print('차은우'[0])
+#
+# 중고차=['k5', 'white', 5000]
+# 중고차[1]='black'
+# print(중고차)
+#
+# 중고차2={
+#     'brand':'bmw',
+#     'model':'520d'
+# }
+# 중고차2['brand']=123
+# print(중고차2['brand'])
 
-fullname='슈퍼 에이전트 하이퍼 초필살 드래곤'
-print(fullname)
-print('차은우'[0])
 
-중고차=['k5', 'white', 5000]
-중고차[1]='black'
-print(중고차)
+# 재고량 = 10
+#
+# if 재고량>0 :
+#     print('지금 주문 가능합니다')
+# else:
+#     print('품절되었습니다')
 
-중고차2={
-    'brand':'bmw',
-    'model':'520d'
-}
-중고차2['brand']=123
-print(중고차2['brand'])
+재고=['K5', 'BMW', 'Tico']
+if 'K5' in 재고:
+    print('주문 가능합니다.')
