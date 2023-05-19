ae18dd1e52fede1b713f57f1e94bd1d744c5c31a
김민지 <wehouse201@naver.com>
2023-05-19 20:29:41 +0900

    program

--- apple.py
+++ apple.py
@@ -39,27 +39,65 @@
 # for i in 중고차들:
 #     print(i)
 
-def solution(numbers):
-    answer = []
-    for i in range(len(numbers)):
-        a=numbers
-        count = 0
+# def solution(numbers):
+#     answer = []
+#     for i in range(len(numbers)):
+#         a=numbers
+#         count = 0
+#
+#         for j in range(i+1, len(a)):
+#             if a[i]<a[j]:
+#                 answer.append(a[j])
+#                 break
+#             else:
+#                 count+=1
+#                 print(a[j], j,i, count)
+#                 if count==len(a)-(i+1):
+#                     answer.append(-1)
+#                     break
+#
+#         if a[i] == a[-1]:
+#             answer.append(-1)
+#     print(answer)
+#     return answer
+#
+# re=list(map(int, input().split()))
+# solution(re)
+#
+# def solution(s):
+#     s.sort()
+#     print(s)
+#     max=s[-1]
+#     min=s[0]
+#     answer = '{} {}'.format(min, max)
+#     return answer
+#
+# a=list(input().split())
+# print(solution(a))
 
-        for j in range(i+1, len(a)):
-            if a[i]<a[j]:
-                answer.append(a[j])
-                break
-            else:
-                count+=1
-                print(a[j], j,i, count)
-                if count==len(a)-(i+1):
-                    answer.append(-1)
-                    break
+# def solution(s):
+#     for i in range(len(s)):
+#         if not s[i][0].isdecimal():
+#             s[i] = s[i][0].upper() + s[i][1:].lower()
+#     answer = ' '.join(s)
+#     return answer
+#
+# a=input().split()
+# print(solution(a))
+#
+# plus = [0 for i in range(len(A))]
+# print(plus)
 
-        if a[i] == a[-1]:
-            answer.append(-1)
-    print(answer)
-    return answer
+# n=int(input())
+# arr = [1, 1]
+#
+# for i in range(n):
+#     arr.append(arr[i] + arr[i + 1])
+#
+# print(arr[n - 1]%1234567)
 
-re=list(map(int, input().split()))
-solution(re)
+from collections import deque
+
+a=list(input())
+for i in a:
+    i
